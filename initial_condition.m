4012162f1353ce78e048fe945637b4ca6907417c
Jordan Scarrott <jordanscarrott@gmail.com>
2022-04-18 11:45:01 +0200

    Delete initial_condition.m
    Not needed after addition of InitialCondition.m class

--- initial_condition.m
+++
@@ -1,9 +0,0 @@
-
-
-function n = initial_condition(n,A0,x,y)
-%     n = initial_gaussian_2d(n,A0);
-%     n = initial_plane_wave(n,A0);
-%     n = initial_sech(n,A0,0);
-    n = initial_exp(n,A0,x,y);
-end
-
